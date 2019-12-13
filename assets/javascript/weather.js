3832f7e59a0150c4be36d03072d39343012d9fa9
Gina Lucy <53538316+grlucy@users.noreply.github.com>
2019-12-13 12:43:50 -0500

    created searchBtn click event

--- assets/javascript/weather.js
+++ assets/javascript/weather.js
@@ -1,1 +1,23 @@
 // API Key: bccd5fad3b0259856da508d996025871
+
+$(document).ready(function() {
+  // Declare variables
+  const cityInputEl = $("#citySearch");
+  const searchBtn = $("#searchBtn");
+  const searchHistoryDiv = $("#cityHistory");
+  const cityAndDateEl = $("#cityAndDate");
+  const currentTempEl = $("#currentTemp");
+  const currentHumidityEl = $("#currentHumidity");
+  const currentWindEl = $("#currentWind");
+  const currentUVEl = $("#currentUV");
+
+  // Add click event to search button
+  searchBtn.on("click", function() {
+    event.preventDefault();
+    // Get city from text input
+    let city = cityInputEl.val();
+    console.log(city);
+    // clear text input
+    cityInputEl.val("");
+  });
+});
